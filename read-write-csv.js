2fdcbf3e127b4424f54fd477ae4454d85b962546
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-04-21 17:44:35 -0600

    -a

--- read-write-csv.js
+++ read-write-csv.js
@@ -1,8 +1,11 @@
 const csv = require("csv-parser");
+const { createObjectCsvWriter } = require("csv-writer");
 const fs = require("fs");
-const path = "data/data.csv";
-const langPath = "data/languages.csv";
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const path = "data/usedCars.csv";
+const carPath = "data/carData.csv";
 
+// Line break
 /* fs.createReadStream(path)
 	.pipe(csv())
 	.on("data", (row) => {
@@ -11,18 +14,67 @@ const langPath = "data/languages.csv";
 
 	.on("end", () => {
 		console.log("Done parsing");
-	}); */
+	});
 
-// Line break
-
-fs.createReadStream(langPath)
+ fs.createReadStream(langPath)
 	.pipe(csv())
 	.on("data", (row) => {
-		if (row.language === "French") {
-			console.log(row);
+		if (row.language == "French") {
+			console.log(row.first_name + " " + row.last_name);
 		}
 	})
 
 	.on("end", () => {
 		console.log("These fools are french");
+	}); */
+
+//Line Break
+
+/* const csvWriter = createObjectCsvWriter({
+	path: path,
+	header: [
+		{ id: "name", title: "First Name" },
+		{ id: "age", title: "Age" },
+	],
+});
+
+const data = [
+	{ name: "Nolan Higgins ", age: 29 },
+	{ age: 29, name: "Paige Higgins " }, //NOTE: order doesn't matter becaus ethe header sets the order
+];
+
+csvWriter.writeRecords(data).then(() => {
+	console.log("done");
+}); */
+
+//Line Break
+
+fs.createReadStream(path)
+	.pipe(csv())
+	.on("data", (row) => {
+		if (row.year >= 2010) {
+			console.log(row.make, row.model, row.year);
+		}
+	})
+
+	.on("end", () => {
+		console.log("Done parsing");
 	});
+
+const csvWriter = createObjectCsvWriter({
+	path: carPath,
+    header:[
+        {id:"make", title: "Make"},
+        {id: "model", title: "Model"},
+        {id: "year", title: "Year"},
+    ],
+});
+
+class car {
+	constructor(make, model, year){
+		    this.make = this.make,
+    this.model = this.model,
+
+	}
+}
+
