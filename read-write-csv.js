0b18189f462f494b7cacaa77d21f5d21e784942a
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-04-21 08:44:47 -0600

    -a

--- read-write-csv.js
+++ read-write-csv.js
@@ -1,13 +1,28 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const path = "data/data.csv";
+const langPath = "data/languages.csv";
 
-fs.createReadStream(path)
+/* fs.createReadStream(path)
 	.pipe(csv())
 	.on("data", (row) => {
-		console.log(`${row}`);
+		console.log(row);
 	})
 
 	.on("end", () => {
 		console.log("Done parsing");
+	}); */
+
+// Line break
+
+fs.createReadStream(langPath)
+	.pipe(csv())
+	.on("data", (row) => {
+		if (row.language === "French") {
+			console.log(row);
+		}
+	})
+
+	.on("end", () => {
+		console.log("These fools are french");
 	});
