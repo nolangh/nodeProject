b213588dfbffa9f2c864f222ee4ab7895ac544ce
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-04-21 08:18:32 -0600

    -a

--- read-write-csv.js
+++ read-write-csv.js
@@ -1,2 +1,13 @@
 const csv = require("csv-parser");
 const fs = require("fs");
+const path = "data/data.csv";
+
+fs.createReadStream(path)
+	.pipe(csv())
+	.on("data", (row) => {
+		console.log(`${row}`);
+	})
+
+	.on("end", () => {
+		console.log("Done parsing");
+	});
